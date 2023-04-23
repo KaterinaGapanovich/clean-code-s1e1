538102ac714cca2149166c6a561b707a20fa66df
Katerina Gapanovich <katerina.kulik@inbox.ru>
2023-04-24 01:12:40 +0300

    fix(extended-2.1): using BEM notation to generate class names

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("edit-mode");
+    var containsClass=listItem.classList.contains("todo-block__edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -106,7 +106,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("edit-mode");
+    listItem.classList.toggle("todo-block__edit-mode");
 };
 
 
